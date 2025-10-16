59923a0038d75c20896f1432ab191c8322dc1206
Drunkdoer16 <drunkdoer16@gmail.com>
2025-10-16 09:49:53 +0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Firebase bağlantı ayarların
+export const firebaseConfig = {
+  apiKey: "AIzaSyBd0S0Nv5glEa_zSUBRl7DkfA8PE0L0zTM",
+  authDomain: "kelimatik-7def6.firebaseapp.com",
+  projectId: "kelimatik-7def6",
+  storageBucket: "kelimatik-7def6.firebasestorage.app",
+  messagingSenderId: "545830068045",
+  appId: "1:545830068045:web:972cc154ce551bfd2d2104"
+};
